7448ea5df0dffd2de38fb4e58ef3c6cf0a1c1f4b
ANA MENDEZ PEREZ <id05294@tid.es>
2024-11-07 12:28:40 +0100

    fixed return carriage and newline

--- src/main/java/com/telefonica/cose/provenance/XMLFileManagement.java
+++ src/main/java/com/telefonica/cose/provenance/XMLFileManagement.java
@@ -33,8 +33,10 @@ public class XMLFileManagement {
 
 		byte[] content;
 		String canonicalizedFile = null;
+		String preprocessedXML = null;
 		try {
-			content = xmlYANG.getBytes();
+			preprocessedXML = xmlYANG.replaceAll(">\\s+<", "><");
+			content = preprocessedXML.getBytes();
 			ByteArrayOutputStream output = new ByteArrayOutputStream();
 
 			Canonicalizer canonicalizer = Canonicalizer.getInstance(Canonicalizer.ALGO_ID_C14N_EXCL_OMIT_COMMENTS);
